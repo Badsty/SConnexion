5304a1404df12aaa7725786cf8d9fe024b421d3a
SLW badsty <111683903+Badsty@users.noreply.github.com>
2022-10-31 18:42:32 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+author 'badsty#7436'
+description 'SConnexion by badsty'
+version '1.0'
+
+game 'gta5'
+
+shared_scripts {
+    'shared/sh_config.lua',
+}
+
+server_scripts {
+    'server/sv_sconnexion.lua'
+}
---
+++ shared/sh_config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+-- Nom du serveur --
+Config.ServerName = "MConnexion"
+
+-- URL Bannière / non obligatoir --
+Config.ServerBannerURL = ""
+
+-- URL Discord --
+Config.DiscordURL = "https://discord.gg/"
